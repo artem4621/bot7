ba96c24e787fcddaf7ab445354c0bbd5a84d1e58
artem4621 <karasevartem674@gmail.com>
2023-07-12 13:53:53 +0700

    shop

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -3,9 +3,12 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from callback.animals import AnimalsCallback
 
-phone = InlineKeyboardButton(text='Телефонов', callback_data=AnimalsCallback(animal='Телефон', count=4).pack())
-laptop = InlineKeyboardButton(text='Ноутбуков', callback_data=AnimalsCallback(animal='Ноутбук', count=5).pack())
+phone = InlineKeyboardButton(text='Телефоны', callback_data=AnimalsCallback(animal='Телефон', count=4).pack())
+laptop = InlineKeyboardButton(text='Ноутбуки', callback_data=AnimalsCallback(animal='Ноутбук', count=5).pack())
+mouse = InlineKeyboardButton(text='Мышки', callback_data=AnimalsCallback(animal='Мышка', count=12).pack())
+headphones = InlineKeyboardButton(text='Наушники', callback_data=AnimalsCallback(animal='Наушники', count=44).pack())
 
 phone_laptop_keyboard = InlineKeyboardMarkup(inline_keyboard=[
     [phone, laptop],
+    [mouse, headphones]
 ])
